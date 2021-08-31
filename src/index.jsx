171d3766b57549dbe5fe18fdc15134d9ff67bcce
Alex Anson <1alex.anson@gmail.com>
2021-08-31 17:54:10 -0400

    state and lifecycle

--- src/index.jsx
+++ src/index.jsx
@@ -228,7 +228,49 @@ import ReactDOM from 'react-dom';
 //   <Comment author={comment.author} date={comment.date} text={comment.text} />,
 //   document.getElementById('root')
 // );
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+// ReactDOM.render(<Clock />, document.getElementById('root'));
+
+// function App() {
+//   return (
+//     <div>
+//       <Clock />
+//       <Clock />
+//       <Clock />
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 const element = <h1>wanted to make vsc stop yelling at me</h1>;
 ReactDOM.render(element, document.getElementById('root'));
-// ReactDOM.render(<App />, document.getElementById('root'));
